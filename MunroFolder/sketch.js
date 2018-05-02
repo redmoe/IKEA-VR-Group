145c58ae86c87795ff577638330f288ac3b01969
Munro Hoberman <31392444+redmoe@users.noreply.github.com>
2018-05-01 20:58:39 -0400

    LookatBookshelf

--- MunroFolder/sketch.js
+++ MunroFolder/sketch.js
@@ -10,8 +10,8 @@ AFRAME.registerComponent('modely', {
     var els = this.el;  
     var sceneEl = document.querySelector('a-scene');
 	els.addEventListener('mouseenter', function () {
-		sceneEl.querySelector('#texty').setAttribute('value', 'This a beutiful mahongy chair and stuff.');
-				sceneEl.querySelector('#texty').setAttribute('fook', 'fooky');
+		sceneEl.querySelector('#texty').setAttribute('value', 'This bookshelf is made of recyclable code. Environmentally friendly!');
+		sceneEl.querySelector('#texty').setAttribute('fook', 'fooky');
 		console.log(sceneEl.querySelector('#texty').getAttribute("fook"));  
 
 		console.log(sceneEl.querySelector('#texty').getAttribute("value"));  
