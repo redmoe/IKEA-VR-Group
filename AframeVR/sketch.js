c46f6dfe09cfe59f6700ec9ee600e1a2809ad64c
Munro Hoberman <31392444+redmoe@users.noreply.github.com>
2018-05-01 21:12:52 -0400

    Removed non assets

--- AframeVR/sketch.js
+++ AframeVR/sketch.js
@@ -1,10 +1,24 @@
-AFRAME.registerComponent('bus', {
+var targetEl;
+AFRAME.registerComponent('texty', {
   init: function () {
-    var targetEl = this.el;  
+    targetEl = this.el;  
+
   }
 });
+AFRAME.registerComponent('modely', {
+  init: function () {
+    var els = this.el;  
+    var sceneEl = document.querySelector('a-scene');
+	els.addEventListener('mouseenter', function () {
+		sceneEl.querySelector('#texty').setAttribute('value', 'This bookshelf is made of recyclable code. Environmentally friendly!');
+		sceneEl.querySelector('#texty').setAttribute('fook', 'fooky');
+		console.log(sceneEl.querySelector('#texty').getAttribute("fook"));  
 
+		console.log(sceneEl.querySelector('#texty').getAttribute("value"));  
+	});
+  }
+});
 function setup() {
-	var el = document.querySelector('#mario');
+
 	
 }
