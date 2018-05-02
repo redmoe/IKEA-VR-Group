3dcb202924fd7f2250c66d3cfb321887cc82ab60
Munro Hoberman <31392444+redmoe@users.noreply.github.com>
2018-05-01 21:43:18 -0400

    See multiple objects

--- AframeVR/sketch.js
+++ AframeVR/sketch.js
@@ -1,3 +1,6 @@
+var descriptions = ["This bookshelf is made of recyclable code. Environmentally friendly!","Hey look at me!","Third Wave"];
+var intObjs = 3	;
+var num = 0;
 var targetEl;
 AFRAME.registerComponent('texty', {
   init: function () {
@@ -5,19 +8,26 @@ AFRAME.registerComponent('texty', {
 
   }
 });
-AFRAME.registerComponent('modely', {
-  init: function () {
-    var els = this.el;  
-    var sceneEl = document.querySelector('a-scene');
-	els.addEventListener('mouseenter', function () {
-		sceneEl.querySelector('#texty').setAttribute('value', 'This bookshelf is made of recyclable code. Environmentally friendly!');
-		sceneEl.querySelector('#texty').setAttribute('fook', 'fooky');
-		console.log(sceneEl.querySelector('#texty').getAttribute("fook"));  
+// 'This bookshelf is made of recyclable code. Environmentally friendly!'
+for (var i = 0; i < intObjs; i++) {
+	AFRAME.registerComponent('modely'+i, {
+	  init: function () {
+	    var els = this.el;  
+	    var ind = num;
+	    var sceneEl = document.querySelector('a-scene');
+		els.addEventListener('mouseenter', function () {
+			console.log(num);
+			sceneEl.querySelector('#texty').setAttribute('value', descriptions[ind]);
+		});
+		num++;
 
-		console.log(sceneEl.querySelector('#texty').getAttribute("value"));  
+	  }
 	});
-  }
-});
+			// 	console.log(num);
+
+			// num++;
+
+}
 function setup() {
 
 	
